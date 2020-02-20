e6bbd3a974aea9da2b5f83bb6d093c22890b39d5
Daniel Bok <daniel.bok@outlook.com>
2020-02-20 22:22:18 +0800

    Initial app setup configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store, { history } from "@/infra/store";
+import "antd/dist/antd.min.css";
+import { ConnectedRouter } from "connected-react-router";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
